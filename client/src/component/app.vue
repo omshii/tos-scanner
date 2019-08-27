3e2507e47c8304db958fdb22116210b44805aa0e
Mohammed Jamil <mjamil90@stuy.edu>
2019-08-26 22:34:21 -0500

    testing bootstrap-vue

--- client/src/component/app.vue
+++ client/src/component/app.vue
@@ -1,11 +1,22 @@
 <template>
     <h1>ToS scanner!</h1>
 </template>
-
-<style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap');
-
-* {
-    font-family: "IBM Plex Sans";
+<script>
+export default {
+  name: "app"
 }
+</script>
+
+<style>
+    #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    }
+
+    @import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+    @import "../../node_modules/bootstrap-vue/dist/bootstrap-vue.css";
 </style>
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,10 @@
 import Vue from "vue";
 import App from "./component/app.vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 
 const app = new Vue({
     el: "#app",
